e67efc6242672521919469c14afb3d34b0aa36ba
aket0 <moynehubert@gmail.com>
2024-02-12 17:07:59 +0100

    add a function for special char, and modifying Chechvalue() for a better score and error cont

--- script.js
+++ script.js
@@ -1,67 +1,90 @@
+let lettersGuessed = [];
+
+function removeAccents(str) {
+    return str.replace(/[àáâãäåçèéêëìíîïñòóôõöùúûüýÿ]/g, function(matched){
+      return accentMap[matched];
+    });
+  }
+
 function randomWord(arr){
-    return arr[Math.floor(Math.random() * arr.length)];
+    return removeAccents(arr[Math.floor(Math.random() * arr.length)]);
 }
+
 
 function checkValue(value, mysteryWord, letterSpans) {
+    value = removeAccents(value.toLowerCase());
+
     let foundIndexes = [];
     for (let i = 0; i < mysteryWord.length; i++) {
-        if (mysteryWord[i] === value) {
+        let letterWithoutAccent = removeAccents(mysteryWord[i].toLowerCase());
+        if (letterWithoutAccent === value) {
             foundIndexes.push(i);
-        } 
+        }
     }
-
-    if (foundIndexes.length > 0) {
-        
-        foundIndexes.forEach(index => {
-            letterSpans[index].innerText = value;
-            point += 1;
-            console.log("Points:", point);
-        });
-    } else {
-        console.log("WRONG");
+   
+    if (lettersGuessed.includes(value)) {
+        console.log("Already guessed this letter.");
         errorCompt += 1;
         console.log("Errors:", errorCompt);
-        switch (errorCompt) {
-            case 1:
-                img.src = "./Images/2.png"
-                break;
-            case 2:
-                img.src = "./Images/3.png"
-                break;
-            case 3:
-                img.src = "./Images/4.png"
-                break;
-            case 4:
-                img.src = "./Images/5.png"
-                break;
-            case 5:
-                img.src = "./Images/6.png"
-                break;
-            case 6:
-                img.src = "./Images/7.png"
-                break; 
-            case 7:
-                img.src = "./Images/8.png"
-                break;
-            case 8:
-                img.src = "./Images/9.png"
-                break;
-            case 9:
-                img.src = "./Images/10.png"
-                divSpan.style.display = "none";
-                tryBtn.style.display = "none";
-                input.style.display = "none";
-                let status = document.createElement("h2");
-                status.innerHTML = "😨💀 HANGED 💀😨"
-                status.style.fontFamily = "Protest Riot"
-                status.style.fontSize = "2em";
-                retrybtn.style.backgroundColor = "#F4E029"
-                row.appendChild(status)
-                row.appendChild(retrybtn)
-                break;
-                   
+    } else {
+        if (foundIndexes.length > 0) {
+            foundIndexes.forEach(index => {
+                letterSpans[index].innerText = value;
+                point += 1;
+                console.log("Points:", point);
+            });
+        } else {
+            console.log("WRONG");
+            errorCompt += 1;
+            console.log("Errors:", errorCompt);
+        }
+        
+        lettersGuessed.push(value);
     }
-}
+
+    
+    switch (errorCompt) {
+        case 1:
+            img.src = "./Images/2.png"
+            break;
+        case 2:
+            img.src = "./Images/3.png"
+            break;
+        case 3:
+            img.src = "./Images/4.png"
+            break;
+        case 4:
+            img.src = "./Images/5.png"
+            break;
+        case 5:
+            img.src = "./Images/6.png"
+            break;
+        case 6:
+            img.src = "./Images/7.png"
+            break; 
+        case 7:
+            img.src = "./Images/8.png"
+            break;
+        case 8:
+            img.src = "./Images/9.png"
+            break;
+        case 9:
+            img.src = "./Images/10.png"
+            divSpan.style.display = "none";
+        tryBtn.style.display = "none";
+        input.style.display = "none";
+        let stat = document.createElement("h2");
+        stat.innerHTML = "😨💀 HANGED ! 💀😨"
+        stat.style.fontFamily = "Protest Riot"
+        stat.style.fontSize = "2em";
+        retrybtn.style.backgroundColor = "#F4E029"      
+        row.appendChild(stat)
+        row.appendChild(retrybtn)
+            break;
+        default:
+            img.src = "./Images/default.png";
+            break;
+    }
 
     if (point === mysteryWord.length) {
         win = true
@@ -70,6 +93,7 @@ function checkValue(value, mysteryWord, letterSpans) {
     }
 }
 
+  
 function checkWin(win){
     if(win){
         divSpan.style.display = "none";
